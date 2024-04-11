1ce66685322bdef38b2f0b70fa7db73a17a09a12
Fuuma <masa81hiro69@gmail.com>
2024-04-11 11:34:48 +0900

    add: ec2のインスタンスに名前を追加

--- chapter1/modules/ec2/main.tf
+++ chapter1/modules/ec2/main.tf
@@ -4,11 +4,13 @@
 # EC2インスタンスの作成
 #----------------------------------------
 resource "aws_instance" "apache" {
-  name                   = var.name
   ami                    = "ami-09d28faae2e9e7138" # Amazon Linux 2
   instance_type          = "t2.micro"
   subnet_id              = var.subnet_id
   vpc_security_group_ids = [var.security_group_id]
+  tags = {
+    Name = var.name
+  }
   user_data              = <<-EOF
     #! /bin/bash
     sudo yum install -y httpd
