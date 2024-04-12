a5780dfb9cc7817f91b7883de6ae522a32f8d52d
Fuuma <masa81hiro69@gmail.com>
2024-04-12 14:07:53 +0900

    fix: ローカルに保存される形式をopenssh形式にした

--- chapter1/modules/key-pair/main.tf
+++ chapter1/modules/key-pair/main.tf
@@ -10,6 +10,6 @@ resource "aws_key_pair" "my_keypair" {
 # ローカルファイルに秘密鍵を生成
 resource "local_file" "private_key" {
   filename        = "./keys/${var.name}.pem"
-  content         = tls_private_key.my_keypair.private_key_pem # Terraformで生成した秘密鍵の内容を取得
+  content         = tls_private_key.my_keypair.private_key_openssh # Terraformで生成した秘密鍵の内容を取得
   file_permission = "400"                                      // 400パーミッションを設定
 }
