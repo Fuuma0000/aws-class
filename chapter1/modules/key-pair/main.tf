77a78d84c5dd85a3684c150c6366b99d6e099ae9
Fuuma <masa81hiro69@gmail.com>
2024-04-13 21:15:42 +0900

    clean: terraform fmt

--- chapter1/modules/key-pair/main.tf
+++ chapter1/modules/key-pair/main.tf
@@ -9,7 +9,7 @@ resource "aws_key_pair" "my_keypair" {
 
 # ローカルファイルに秘密鍵を生成
 resource "local_file" "private_key" {
-  filename        = "./keys/${var.name}.pem"
-  content         = tls_private_key.my_keypair.private_key_openssh # Terraformで生成した秘密鍵の内容を取得
-  file_permission = "400"                                      // 400パーミッションを設定
+  filename        = "./keys/${var.name}.pem" # Terraformで生成した秘密鍵の内容を取得
+  content         = tls_private_key.my_keypair.private_key_openssh
+  file_permission = "400" // 400パーミッションを設定
 }
